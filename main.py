e46367e1f890974e6af93e4a6513352068ea1e1a
FarhanTausif <bsse1440@iit.du.ac.bd>
2025-10-14 16:41:37 +0600

    Re-engineer propmt & Fix env path

--- main.py
+++ main.py
@@ -5,10 +5,9 @@ import os
 from dotenv import load_dotenv
 
 # Load environment variables from .env file
-load_dotenv()
+load_dotenv(dotenv_path=".env.local")
 
 # Initialize Gemini client (add api_key if not using env variable)
-# client = genai.Client(GEMINI_API_KEY=os.getenv("GEMINI_API_KEY"))
 client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
 # Paths to your PDF files
@@ -20,14 +19,7 @@ resume_file = client.files.upload(file=resume_path, config={"mime_type": "applic
 jd_file = client.files.upload(file=jd_path, config={"mime_type": "application/pdf"})
 
 # Prompt for ATS evaluation
-prompt = """
-You are an ATS evaluation engine.
-Compare the candidate's resume against the job description.
-1. Provide a similarity score (0-100)
-2. List main matching skills
-3. List missing skills or requirements
-4. Short explanation of the score
-"""
+prompt=os.getenv("ATS_PROMPT")
 
 # Generate content using Gemini
 response = client.models.generate_content(
