672d3e2af10a58e58089dfc4fe55e044113ebf0a
Piccolo7210 <bsse1405@iit.du.ac.bd>
2025-10-15 18:16:50 +0600

    CV score system tested on springboot, with the help of Spring Ai and gemini

---
+++ ATS-Plus/src/main/java/com/example/ATS_Plus/AtsPlusApplication.java
@@ -0,0 +1,13 @@
+package com.example.ATS_Plus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtsPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtsPlusApplication.class, args);
+	}
+
+}
---
+++ ATS-Plus/src/test/java/com/example/ATS_Plus/AtsPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ATS_Plus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtsPlusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
